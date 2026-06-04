306678d9d398b5e99c97cd8733cf6ddfb5258859
Anmol jain <abhianmol646@gmail.com>
2026-06-04 13:40:35 +0530

    Add files via upload

---
+++ DAY-8/repeatedpattern.cpp
@@ -0,0 +1,22 @@
+// Write a program to Print half pyramid pattern.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int n;
+    cout << "ENTER THE VALUE ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <=i; j++)
+        {
+            cout << i ;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
